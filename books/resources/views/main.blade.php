710853784a9afbb25d4c0ec19c18c1a59f6d75cf
Junior Paiva <juniorpaiva95@gmail.com>
2016-10-05 22:58:50 -0300

    Pasta incluida sem querer.

--- books/resources/views/main.blade.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-  @include('layouts_admin.head')
-  <body>
-    @include('layouts_admin.menu')
-    @yield("content")
-  </body>
-</html>
--- books/resources/views/welcome.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('main')
-@section('title','Admin - Dashboard')
-@section('content')
-
-@endsection
